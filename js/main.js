6b29eb25577cb2d333f8c52ba13bde02d7bc774a
Meter568 <mmrrff2099@gmail.com>
2025-04-03 10:23:04 +0300

    Pr7

--- js/main.js
+++ js/main.js
@@ -10,9 +10,24 @@ var app = new Vue({
         {id:7, title:"TAG 1006 (TAG 898)", short_text:"Sweet pepper Determinate Red Elongated", image:"../images/pepper_7.png", color1:"red", Color1:"Red"},
         {id:8, title:"TAG 1007 (TAG 816)", short_text:"Sweet pepper Determinate Red Elongated", image:"../images/pepper_8.png", color1:"red", Color1:"Red"}],
         product: {},
+        cart: [],
+        contactFields: {
+            name: "",
+            company_name: "",
+            position: "",
+            city: "",
+            country: "",
+            telephone: "",
+            email: "",
+            you: "",
+            other: "",
+            interest: "",
+            code: ""
+        },
+        forumVisible: 0,
         btnVisible: 0,
         name:[{Title:"Sweet pepper", Titles:"Sweet peppers", titles:"sweet peppers", title:"sweet pepper"}],
-        banner:[{image:"../images/banner-papper.png"}]
+        banner:[{image:"../images/banner-papper.png"}],
     },
     methods:{
         getProduct:function(){
@@ -37,16 +52,56 @@ var app = new Vue({
                 cart.push(id);
                 window.localStorage.setItem('cart',cart.join());
                 this.btnVisible=1;
-            }
+            }   
         },
         checkInCart:function(){
             if(this.product && this.product.id && window.localStorage.getItem('cart').split(',').indexOf(String(this.product.id))!=-1){
                 this.btnVisible = 1;
             }
+        },
+        getCart: function() {
+            this.cart = []; 
+            if (window.localStorage.getItem('cart')) {
+                let storedCart = window.localStorage.getItem('cart').split(',');
+                for (let id of storedCart) {
+                    let foundProduct = this.products.find(product => product.id == id);
+                    if (foundProduct) {
+                        this.cart.push(foundProduct);
+                    }
+                }
+            }
+        },
+        removeFromCart: function(id) {
+            this.cart = this.cart.filter(product => product.id !== id);
+            
+            let storedCart = window.localStorage.getItem('cart') ? window.localStorage.getItem('cart').split(',') : [];
+            storedCart = storedCart.filter(cartId => cartId != id);
+            window.localStorage.setItem('cart', storedCart.join(','));
+        },
+        makeOrder:function(){
+            if (!this.contactFields || Object.keys(this.contactFields).length === 0) {
+                alert('There is no user data to display.');
+                return;
+            }
+            if (!this.contactFields.code) {
+                alert('Enter the code to continue.');
+                return;
+            }
+            if (this.contactFields.code !== "7cj3") {
+                alert('Incorrect code. Please enter again.');
+                return;
+            }
+            alert('Message sent successfully!');
+            this.forumVisible = 1;
+            if (window.cart) {
+                cart.length = 0;
+            }
+            localStorage.removeItem('cart');
         }
     },
     mounted:function(){
         this.getProduct();
         this.$nextTick(this.checkInCart);
+        this.getCart();
     }
 });
